2e7a8e82c70e72480a59ce4229f46e216daca807
GitHub Action <action@github.com>
2024-09-09 00:04:14 +0000

    Initialize template repository proper
    Set package name to com.github.subat0m1c.hatecheaters
    Set project name to HateCheaters
    Set modid to hatecheaters

--- src/main/java/com/github/subat0m1c/hatecheaters/ExampleMod.java
+++ src/main/java/com/github/subat0m1c/hatecheaters/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.subat0m1c.hatecheaters;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "hatecheaters", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/subat0m1c/hatecheaters/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/subat0m1c/hatecheaters/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.subat0m1c.hatecheaters.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
